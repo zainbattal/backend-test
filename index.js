3fd48e9f6343ea5c74d2d662674782ec2ed0dd50
zainbattal <battalzein@gmail.com>
2025-06-04 12:22:14 +0300

    backend git

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+let express = require("express");
+let app = express();
+let cors = require("cors");
+const pool = require("./db");
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// add a name
+
+app.post("/", async (req, res) => {
+  try {
+    const { inputValue } = req.body;
+    const newName = await pool.query("INSERT INTO names (name) VALUES($1)", [
+      inputValue,
+    ]);
+    console.log("name added!");
+  } catch (error) {
+    console.error(error);
+  }
+  res.send("");
+});
+
+// get names
+
+app.get("/names", async (req, res) => {
+  const names = await pool.query("SELECT * FROM names");
+  res.json(names.rows);
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at port: ${PORT}`);
+});
