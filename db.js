eb678335f750d5f9a86fc69ba02e8db7875dff0f
zainbattal <battalzein@gmail.com>
2025-06-05 14:35:53 +0300

    ipv4

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false }, // Required for Supabase
+  connectionTimeoutMillis: 5000, // Timeout after 5 seconds
+  family: 4,
 });
 
 module.exports = pool;
