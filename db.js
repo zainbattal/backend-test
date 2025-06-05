d308c7c662c25d1b00f1b339807939e45b20e8e3
zainbattal <battalzein@gmail.com>
2025-06-05 15:19:58 +0300

    switch to render db

--- db.js
+++ db.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // Required for Supabase
-  connectionTimeoutMillis: 5000, // Timeout after 5 seconds
-  family: 4,
 });
 
 module.exports = pool;
